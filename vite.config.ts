204cdfb075ccc7e94d2462d408569c33742d341c
shaikvaliyababji <2300039117@kluniversity.in>
2025-12-29 21:51:04 +0530

    modified json

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: process.env.VITE_BASE_PATH || "/Plant_Ecommers",
+  base: process.env.NODE_ENV === "production" ? "/" : "/Plant_Ecommers/",
 });
+
